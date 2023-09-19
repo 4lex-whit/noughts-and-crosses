863b74eb51c5799b53aca999308edd85d6825755
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 10:54:12 +0100

    Add main class

---
+++ noughtsAndCrosses/src/com/github/alex_whit/NoughtsAndCrosses.java
@@ -0,0 +1,11 @@
+package com.github.alex_whit;
+
+public class NoughtsAndCrosses {
+	public static final String VERSION = "v0.0.0";
+	
+	public static void main(String[] args) {
+		System.out.println(String.format("""
+				Noughts and Crosses | %s
+				By alex_whit""", VERSION));
+	}
+}
