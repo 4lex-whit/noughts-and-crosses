c25f9972b116ac0e385948ed8548d3be3107422e
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 11:05:42 +0100

    Declare LOGGER and frame variables

--- noughtsAndCrosses/src/com/github/alex_whit/ui/GUI.java
+++ noughtsAndCrosses/src/com/github/alex_whit/ui/GUI.java
@@ -1,5 +1,11 @@
 package com.github.alex_whit.ui;
 
-public class GUI {
+import java.util.logging.Logger;
 
+import javax.swing.JFrame;
+
+public class GUI {
+	private static final Logger LOGGER = Logger.getLogger(GUI.class.getName());
+	
+	private static JFrame frame;
 }
